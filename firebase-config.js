fc559dca4b24667796e3ea0322b3d9b52dedd3aa
felixfelicisharrypottergdr-jpg <felixfelicisharrypottergdr@gmail.com>
2026-09-16 20:29:38 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+ apiKey: "AIzaSyCvd56bSEL02s2OY2PSk7uyFX-CYRB4Vz0",
+ authDomain: "conteggi-eee1b.firebaseapp.com",
+ projectId: "conteggi-eee1b",
+ storageBucket: "conteggi-eee1b.firebasestorage.app",
+ messagingSenderId: "481320219586",
+ appId: "1:481320219586:web:87e974d618a302adfadc5b",
+ measurementId: "G-EFHVL4H1XW"
+};
